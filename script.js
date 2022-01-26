c1bb8d518c3d9c78200b2d91d47a83b6a3c94b78
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-25 23:09:14 -0300

    atividade numero 4
    Nesse commit fiz a atividade número quatro

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ function spotify(nome, idade, estiloMusical) {
     console.log(`Olá ${nome}, sua idade é  ${idade} e seu estilo musical favorito é ${estiloMusical}`);
 }
 spotify("Samantha", "28", "rock");
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+function cinema(filme, musica) {
+    console.log(`O melhor filme do ano é  ${filme}, e a melhor música é  ${musica} `);
+}
+cinema("Homem Aranha sem volta para casa", "my universe");
