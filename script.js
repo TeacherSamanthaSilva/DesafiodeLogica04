2c346dc36321a17d748ade525b7b32f45187b541
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-25 22:38:26 -0300

    funcao com nome
    Nesse commit criei a função com nome

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//1 - crie uma função que exiba uma mensagem no console
+
 function olaMundo() {
     console.log("Olá Mundo");
 }
 olaMundo();
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function funcaocomnome(nome) {
+    console.log(`Olá ${nome}`);
+}
+​
+funcaocomNome("Samantha"); 
+ 
