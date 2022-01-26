e7bacfecbb301704d5111be275eaf07c88e90fd7
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-25 22:48:25 -0300

    função spotify
    Nesse commit criei uma função com multiplos parâmetros

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function funcaocomNome(nome) {
 }
 funcaocomNome("Samantha"); 
  
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+function spotify(nome,idade,estiloMusical){
+    console.log(`Olá ${nome}, você tem ${anos} e seu estio musical favorito pe ${estiloMusical}?`
+}
+spotify("Samantha","28","rock");
